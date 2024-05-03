651ef9e7128825fa71207cf0594aca30eb522ac2
theproductcoach <will@productcoach.io>
2024-05-03 13:52:28 +0100

    Trying again

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/Portfolio/',  // Ensure this matches the GitHub Pages subpath
+  build: {
+    outDir: 'dist',  // This should match the directory name you're deploying
+  },
+  // Add other configurations as needed
+});
